12e10e0165a3825762a356084990547ced2e54a2
“Daniel <danielanderson.lopes0@gmail.com>
2024-04-21 15:21:36 -0300

    Primeiro commit

---
+++ src/main/java/br/com/danllopes/reminderapi/ReminderapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.danllopes.reminderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminderapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminderapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/danllopes/reminderapi/ReminderapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danllopes.reminderapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReminderapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
